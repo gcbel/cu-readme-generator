6b93764664ddd023dda36e703d4855bd74b7297e
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-06-20 14:40:57 -0600

    ask questions successfully

--- index.js
+++ index.js
@@ -1,44 +1,81 @@
-// TODO: Include packages needed for this application
+/* DEPENDENCIES */
+const {writeFile} = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
+/* VARIABLES */
+/* All prompts needed for complete readme. */
 const questions = [
     {
-        'type': 'input',
-        'message': 'What is the name of your project?',
-        'name': 'title'
+        "type": "input",
+        "message": "What is the name of your project?",
+        "name": "title"
     },
     {
-        'type': 'input',
-        'message': 'Please describe the project.',
-        'name': 'description',
+        "type": "input",
+        "message": "Please describe the project.",
+        "name": "description"
     },
     {
-        'type': 'input',
-        'message': 'Please describe the installation instructions for the project.',
-        'name': 'installation',
+        "type": "input",
+        "message": "Please describe the installation instructions for the project.",
+        "name": "installation"
     },
     {
-        'type': 'input',
-        'message': 'Please describe how to use the project.',
-        'name': 'usage',
+        "type": "input",
+        "message": "Please describe how to use the project.",
+        "name": "usage"
     },
     {
-        'type': 'input',
-        'message': 'Please describe the contribution guidelines for the project.',
-        'name': 'contributing',
+        "type": "input",
+        "message": "Please describe the contribution guidelines for the project.",
+        "name": "contributing"
     },
     {
-        'type': 'input',
-        'message': 'Please describe the test instructions for the project.',
-        'name': 'test',
+        "type": "input",
+        "message": "Please describe the test instructions for the project.",
+        "name": "tests"
     }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+/* FUNCTIONS */
+/**
+ * Generates text content for README file.
+ * @param {string} fileName Name of file to input text
+ * @param {} data Information from user prompts
+ */
+function writeToFile(fileName, data) {
+    console.log(data);
+    content = `
+        # ${data.title}
+        
+        ## Description
+        ${data.description}
+        
+        ## Installation
+        ${data.installation}
+        
+        ## Installation
+        ${data.installation}
 
-// TODO: Create a function to initialize app
-function init() {}
+        ## Usage
+        ${data.usage}
 
-// Function call to initialize app
+        ## Contributing
+        ${data.contributing}
+
+        ## Tests
+        ${data.tests}
+        `
+    writeFile(filename, content, (err));
+}
+
+/* INITIALIZERS */
+/* Initialize call for user input and readme creation */
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((responses) => writeToFile("mini_README.md", responses));
+}
+
+/* Function call to initialize app */
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,5 @@
 /**
  * Handles generation of a license badge link if license is provided.
- * 
  * @param {string} license
  * @returns {string} Corresponding license badge as a string, empty if no license provided
  */
@@ -11,7 +10,6 @@ function renderLicenseBadge(license) {
 
 /**
  * Returns license badge link.
- * 
  * @param {string} license
  * @returns {string} License badge link as a string
  */
@@ -21,7 +19,6 @@ function renderLicenseLink(license) {
 
 /**
  * Returns the license section of README.
- * 
  * @param {string} license
  * @returns {string} License section of README, empty if no license provided
  */
@@ -31,7 +28,6 @@ function renderLicenseSection(license) {
 
 /**
  * Generates markdown for README.
- * 
  * @param 
  * @returns
  */
